651c7d7c9261f64a5bbf015a959d416ac23042f1
americanman <snowboardracer7@gmail.com>
2019-05-31 00:48:16 -0400

    Serve json object

--- myApp.js
+++ myApp.js
@@ -31,6 +31,7 @@ app.use(express.static(__dirname + '/public/'))
 
 /** 5) serve JSON on a specific route */
 
+app.get('/json', (req, res) => res.json({"message": "Hello json"}))
 
 /** 6) Use the .env file to configure the app */
  
