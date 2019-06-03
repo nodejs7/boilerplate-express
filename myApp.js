d9f779684c11d6d095caae9a57e9653afd75cba9
americanman <snowboardracer7@gmail.com>
2019-06-02 22:56:36 -0400

    Add env to the project

--- myApp.js
+++ myApp.js
@@ -31,11 +31,18 @@ app.use(express.static(__dirname + '/public/'))
 
 /** 5) serve JSON on a specific route */
 
-app.get('/json', (req, res) => res.json({"message": "Hello json"}))
+//app.get('/json', (req, res) => res.json({"message": "Hello json"}))
 
 /** 6) Use the .env file to configure the app */
  
- 
+app.get('/json', (req, res) => {
+  let message = 'Hello json';
+  if(process.env.MESSAGE_STYLE === 'uppercase') {
+    message = message.toUpperCase()
+  }
+  return res.json({"message": message})
+})
+
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
 
