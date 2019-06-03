cc48d719a9046511353c71a15aaf2c9d14b07274
americanman <snowboardracer7@gmail.com>
2019-06-02 23:50:54 -0400

    Add logger with middleware function

--- myApp.js
+++ myApp.js
@@ -4,6 +4,10 @@ var app = express();
 
 // --> 7)  Mount the Logger middleware here
 
+app.use('/', (req, res, next) => {
+  console.log(req.method, req.path, '-', req.ip);
+  next();
+})
 
 // --> 11)  Mount the body-parser middleware  here
 
